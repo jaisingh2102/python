3fd4acdbf49867865683063f7cc2cb2acddd7b01
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-17 23:56:58 +0530

    arithmatic operation

---
+++ arithmatic.py
@@ -0,0 +1,13 @@
+a=100
+b=20
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = a%b
+print("sum of a+b is:", c)
+print("minus of a-b is", d)
+print("multiply of a*b is", e)
+print("divide of a/b is", f)
+print("remainder of a%b is",g)
+print(a ** b) #power operator
