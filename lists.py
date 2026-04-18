da9ac482ed699e7b408651b2cedac8979ca8b12c
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-18 15:34:46 +0530

    lists in the python

---
+++ lists.py
@@ -0,0 +1,16 @@
+# liste:- a build-in data structure that stores multiple elements of different data types in a single variable.
+# It is ordered, mutable, and allows duplicate elements.
+# Lists are defined using square brackets [] and elements are separated by commas.
+a = ["aman", 20, 7.52, "codder"]
+print(type(a))
+print(a)
+print(a[3])
+print(len(a))
+a[3] = "engineer"
+print(a)
+b = a[ :2]
+print(b)
+c = a[0 :]
+print(c)
+d = a[-3:-1]
+print(d)
