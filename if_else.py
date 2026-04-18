34ed4e60d15b571b24b56cbf293dd30dddf60df1
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-18 15:23:10 +0530

    condition

---
+++ if_else.py
@@ -0,0 +1,9 @@
+a = int(input("enter marks of student: "))
+if a>=90:
+    print("grade A")
+elif (a>=80 and a<90):
+    print("grade B")
+elif (a>=70 and a<80):
+    print("grade C")
+else:
+    print("grade D")
