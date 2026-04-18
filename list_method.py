62adfd72d4a4694d7854116a503e08794658c773
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-18 15:33:12 +0530

    methods of the list

---
+++ list_method.py
@@ -0,0 +1,17 @@
+list = [5,7,3,2,9]
+list.append(6) # adds an element to the end of the list
+print(list)
+list.sort() # sorts the list in ascending order
+print(list)
+list.reverse() # reverses the order of the list
+print(list)
+list.insert(2,10) # inserts an element at a specific index
+print(list)
+list.sort()
+print(list)
+list.remove(10) # removes the first occurrence of the specified value
+print(list)
+list.pop() # removes and returns the last element of the list
+print(list)
+list.pop(1) # removes and returns the element at the specified index
+print(list)
