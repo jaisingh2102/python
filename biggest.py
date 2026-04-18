b052caed98bf48d4c28d0c090afd14cb31f92a8b
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-18 14:12:12 +0530

    biggest between 3

---
+++ biggest.py
@@ -0,0 +1,9 @@
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+c = int(input("enter the third number: "))
+if(a>=b and a>=c):
+    print(a, "is the greatest number: ",a)
+elif (b>a and b>=c):
+    print(b, "is the greatest number: ",b)
+else:
+    print("c is the greatest number: ",c)
