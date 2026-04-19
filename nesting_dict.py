555c7b700214ba1233659dbb973d5c52625ef972
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-19 15:38:19 +0530

    inthis program we perform nesting of dictinory

---
+++ nesting_dict.py
@@ -0,0 +1,21 @@
+dict = {
+    "person1" : {
+        "name" : "aman",
+        "age" : 20,
+        "city" : "lucknow",
+        "cgpa" : 7.52
+    },
+    "person2" :
+    {
+        "name" : "ayush",
+        "age" : 22,
+        "city" : "delhi",
+        "cgpa" : 8.1
+
+    }
+}
+print(dict)
+print(dict["person1"])
+print(dict["person1"]["name"])
+print(dict["person2"]["cgpa"])
+print(dict["person2"])
