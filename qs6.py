c16e84a63587d9fcf5a86d687d79fecae12d9e57
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-19 15:42:18 +0530

    practice question no 6

---
+++ qs6.py
@@ -0,0 +1,4 @@
+set = {"python","java","c++","python","javascript","java","python","java","c++","c"}
+length = len(set)
+print(length) #prints the length of the set, which is 5 because sets do not allow duplicate values
+print(set) #prints the whole set, which contains only unique values
