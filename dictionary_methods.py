e4e103143e11c54a1542152b58d84a9ef257e8c9
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-19 15:46:18 +0530

    perform dictionary methods

---
+++ dictionary_methods.py
@@ -0,0 +1,14 @@
+dict = {
+    "name" : "aman",
+    "village" : "naikapar",
+    "age" : 20,
+    "sport" : "cricket",
+    "profession" : "computer engineer",
+}
+print(dict)#prints the whole dictionary
+print(dict.keys())#prints the keys of the dictionary
+print(dict.values())#prints the values of the dictionary
+print(dict.items())#prints the key value pairs of the dictionary
+print(dict.get("sport"))#prints the value of the key "sport"
+dict.update({"hobby": "coding"})#updates the dictionary with the new key value pair
+print(dict)
