c49bd32a3540670dec8538f2326dea09c9cf71cb
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-20 14:45:42 +0530

    perform for function

---
+++ function.py
@@ -0,0 +1,8 @@
+def sum(a,b):#function definition with parameters a and b
+    s = a+b
+    return s
+
+a = int(input("enter first number: "))
+b = int(input("enter second number:"))
+output = sum(a,b)#function call with arguments a and b, the output will be stored in variable output
+print("sum of",a,"and",b,"is",output)
