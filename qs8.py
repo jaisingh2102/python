1b3078a4544b6fad59bdf53f2c2b27ebacd8709d
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-20 14:53:14 +0530

    perform for question8

---
+++ qs8.py
@@ -0,0 +1,8 @@
+set = {9,"9.0"}
+print(set)#prints the set, which contains the integer 9 and the string "9.0" as two distinct elements
+# OR built in methods of set
+set1 = {
+    ("float", 9.0),
+    ("integer", 9),
+}
+print(set1)
