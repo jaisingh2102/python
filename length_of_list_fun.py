bdfe9902b429f53131d856a78f2f787ee7ec12d1
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-21 01:18:58 +0530

    calculate length of the list using function call

---
+++ length_of_list_fun.py
@@ -0,0 +1,7 @@
+cities = ["new york", "dilhi","london","mumbai","paris","sydney"]
+subjects =["java","python","c++","javascript","ruby","go"]
+def calc_length(list):
+    print("the length of the list is", len(list))
+
+calc_length(cities)
+calc_length(subjects)
