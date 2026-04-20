6e6cd683995c6508e48f07b49ffe3da0b2b6ca19
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-21 00:44:35 +0530

    perform avg of 3 number using function call

---
+++ avg_fun.py
@@ -0,0 +1,9 @@
+def calc_avg(a,b,c):
+    avg = (a+b+c)/3
+    return avg
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+c = int(input("enter third number: "))
+output = calc_avg(a, b, c)
+print("the average of", a, b, "and", c, "is", output)
