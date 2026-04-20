6264f242aab62746f1dc75f53af3de55019f7b6b
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-20 14:52:48 +0530

    perform for question7

---
+++ qs7.py
@@ -0,0 +1,8 @@
+marks ={}
+a = int(input("enter the marks of javascript:"))
+marks.update({"javascript": a})
+b = int(input("enter the marks of java: "))
+marks.update({"java" :b})
+c= int(input("enter the marks of python: "))
+marks.update({"python" :c})
+print(marks)
