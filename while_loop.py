36f67362fcf70bc7a2e370aaea90c33a0e392617
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-20 15:06:59 +0530

    perform for sum of n numbers using while loop

---
+++ while_loop.py
@@ -0,0 +1,10 @@
+#while_loop:when we want to execute a block of code repeatedly as long as a certain condition is true,
+#we can use a while loop. The syntax for a while loop is as follows:
+#while condition:
+    # code to be executed
+count = 1
+while count <= 50:
+    print(count)
+    count += 1
+    
+    
