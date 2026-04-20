41ff1fe8fb26b577586fad5db3984f51a8bd881a
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-21 00:56:51 +0530

    perform multiplication of two numbers using function call

---
+++ product_fun.py
@@ -0,0 +1,10 @@
+def calc_product(a , b):
+    c = a * b
+    return c
+
+    
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+output = calc_product(a , b)
+print("the product of", a, "and", b, "is", output)
+
