611856fdb3a6fb991cca47d5fceb1f385e25a0de
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-23 23:32:45 +0530

    calculate factorial with the  help of function call

---
+++ factorial_fun.py
@@ -0,0 +1,10 @@
+#find factorial with the help of function
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+num = int(input("Enter a number: "))
+result = factorial(num)
+print(f"The factorial of {num} is {result}.")
