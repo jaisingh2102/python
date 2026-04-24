196365ecf7af80abbba56004c06220d80b35f86b
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-25 01:11:35 +0530

    print members of the list using recursion

---
+++ list_recursion.py
@@ -0,0 +1,11 @@
+gods = ["rama","krishna","shiva","vishnu","hanuman","ganesha"]
+def print_list(gods):
+    if len(gods) == 0:
+        return
+    else:
+        print(gods[0])
+        print_list(gods[1:])
+
+
+output = print_list(gods)
+print(output)        
