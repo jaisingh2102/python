08dc8f655f6a51407a64a1e5f37f859d3bc4ea3d
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-25 00:26:39 +0530

    count factorial of number 5 using recursion

---
+++ factorial_recursion.py
@@ -0,0 +1,11 @@
+# Factorial of a number using recursion
+def fact(n):
+    if(n==0 or n==1):
+       
+        return 1
+    else:
+        return n * fact(n-1)
+
+
+output = fact(5)
+print(output)
