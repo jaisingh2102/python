956f9deea720371064c73f1863bffa940eadbd14
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-26 22:57:17 +0530

    calculate attendence of the student

---
+++ attendence.py
@@ -0,0 +1,11 @@
+total = int(input("Total classes: "))
+attended = int(input("Classes attended: "))
+
+percentage = (attended / total) * 100
+
+print("Attendance:", percentage, "%")
+
+if percentage >= 75:
+    print("Eligible for exam")
+else:
+    print("Not eligible for exam")
