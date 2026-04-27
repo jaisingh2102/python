e5b09817589b669c8437119a8ee66c66519faf7e
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-27 20:17:24 +0530

    create result of a student

---
+++ result.py
@@ -0,0 +1,18 @@
+name = input("Enter student name: ")
+math = int(input("Math marks: "))
+science = int(input("Science marks: "))
+english = int(input("English marks: "))
+
+total = math + science + english
+percentage = total / 3
+
+print("\nStudent:", name)
+print("Total:", total)
+print("Percentage:", percentage)
+
+if percentage >= 60:
+    print("Result: First Class")
+elif percentage >= 40:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
