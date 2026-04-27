035205c748aed1ff516a6f0563f7e5c16c9784bc
jaisingh2102 <singhamanjay102@gmail.com>
2026-04-27 20:16:10 +0530

    create a shopping bill

---
+++ shoppingbill.py
@@ -0,0 +1,11 @@
+price = int(input("Enter product price: "))
+quantity = int(input("Enter quantity: "))
+
+total = price * quantity
+
+if total >= 5000:
+    discount = total * 0.1
+    total -= discount
+    print("Discount applied:", discount)
+
+print("Final Bill:", total)
