3665c46182c24f5eae9f8ba89c55ce1f91591c79
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-06 23:25:56 +0530

    create inheritence of employees which inherit engineer

--- inheritence1.py
+++ inheritence1.py
@@ -1,13 +1,13 @@
-class employee:
+class employee:# This is the base class 'employee' which has a method 'work'.
     def work(self):
         print("employees are working")
 
 
-class engineer(employee):
+class engineer(employee):# The 'engineer' class inherits from the 'employee' class.
     def code(self):
         print("engineers are doing coding on a project")
 
 
-e = engineer()
+e = engineer()# An instance of the 'engineer' class is created, which can access the 'work' method from the 'employee' class and the 'code' method from the 'engineer' class.
 e.work()
 e.code()                
