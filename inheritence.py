b3c7e74f98a68fafba6aca043161efe88d3fdf01
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-06 23:27:54 +0530

    create inheritence of animal which inherit dog

--- inheritence.py
+++ inheritence.py
@@ -1,12 +1,12 @@
-class animal:
+class animal:# This is the base class 'animal' which has a method 'eat'.
     def eat(self):
         print("animals can eat")
 
-class dog(animal):
+class dog(animal):# The 'dog' class inherits from the 'animal' class.
     def bark(self):
         print("dogs can bark")
 
 
-d = dog()
+d = dog()# An instance of the 'dog' class is created, which can access the 'eat' method from the 'animal' class and the 'bark' method from the 'dog' class.
 d.eat()        
 d.bark()
