9476d0fde48200fb59a11e62e26d74487093d48a
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-06 23:23:31 +0530

    create inheritence of vehicles which inherit car

--- vehicals.py
+++ vehicals.py
@@ -1,11 +1,12 @@
+# This code demonstrates inheritance in Python. The 'car' class inherits from the 'vehicle' class, allowing it to use the 'start' method defined in the 'vehicle' class while also having its own method 'drive'.
 class vehicle:
-    def start(self):
+    def start(self):# This method is defined in the 'vehicle' class and can be used by any class that inherits from it.
         print("vehicle is starting")
 
-class car(vehicle):
+class car(vehicle):# The 'car' class inherits from the 'vehicle' class, meaning it can use the methods defined in 'vehicle' as well as its own methods.
     def drive(self):
         print("car is driving")
 
-c = car()
+c = car()# An instance of the 'car' class is created, which can access both the 'start' method from the 'vehicle' class and the 'drive' method from the 'car' class.
 c.start()
 c.drive()                
