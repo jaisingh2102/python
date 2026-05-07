f67b873181a6527f1d155c1e6819ae7ce7c53f23
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-07 22:41:49 +0530

    create a map code

--- map.py
+++ map.py
@@ -1,5 +1,5 @@
 nums = ["10", "20", "30", "40"]
 
-result = list(map(int, nums))
+result = list(map(int, nums))# The 'map' function is used to apply the 'int' function to each element in the 'nums' list, converting the string representations of numbers into actual integers. The result is a map object that can be converted into a list or iterated over to access the converted values. In this case, we convert it directly into a list.
 
 print("Integers:", result)
