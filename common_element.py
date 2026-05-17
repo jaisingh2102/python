96822f534df2f32339546fdaddf3b43f5e4790c0
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-17 21:19:31 +0530

    update This code finds the common elements between two lists.

--- common_element.py
+++ common_element.py
@@ -1,3 +1,4 @@
+# This code finds the common elements between two lists, arr1 and arr2, and stores them in a new list called common. Finally, it prints the common elements.
 arr1 = [1, 2, 3, 4]
 arr2 = [3, 4, 5, 6]
 
