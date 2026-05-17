458880ddb3c7e7ccaf4c26c521e6d2e274839a96
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-17 21:21:49 +0530

    update This code demonstrates the use of comparison operators in Python.

--- comperation_operator.py
+++ comperation_operator.py
@@ -1,3 +1,4 @@
+# This code demonstrates the use of comparison operators in Python. It compares two variables, a and b, using various comparison operators and prints the results.
 a = 50
 b = 20
 print(a == b)
