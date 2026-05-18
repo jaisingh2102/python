65b51d3e3b2582705a214e45eecd2d51316a3773
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-18 11:33:28 +0530

    update This code demonstrates a recursive countdown function that counts down from a given number to zero.

--- countdown_recur.py
+++ countdown_recur.py
@@ -1,3 +1,4 @@
+# This code demonstrates a recursive countdown function that counts down from a given number to zero, printing each number with a delay of 0.75 seconds.
 import time
 def countdown(n):
     if n<= 0:
