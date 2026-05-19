f94be8e88ffff6171a5916a4893200c2a58d896c
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-19 23:29:16 +0530

    update This program defines a list of sports and a function to print the elements of the list. The function takes a list as an argument and prints its contents. Finally, the function is called with the 'sports' list to display the elements.

--- element_of_list_fun.py
+++ element_of_list_fun.py
@@ -1,4 +1,4 @@
-# This program defines a list of sports and a function to print the elements of the list.
+# This program defines a list of sports and a function to print the elements of the list. The function takes a list as an argument and prints its contents. Finally, the function is called with the 'sports' list to display the elements.
 sports = ["cricket", "football", "tennis", "badminton", "hockey"]
 def print_list(list):
     print("the elements of the list are:", list)
