1bc286d3f41e24d142d6eca4a3c3d21769d46a0a
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-20 22:27:42 +0530

    update This program searches for a specific number in a list and prints its index if found.

--- for2.py
+++ for2.py
@@ -1,3 +1,4 @@
+# This program searches for a specific number in a list and prints its index if found.
 list = [1,4,9,16,25,36,49,64,81,100]
 x = int(input("enter a number to search: "))
 for i in range(len(list)):
