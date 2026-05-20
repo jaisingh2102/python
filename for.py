607c0a68d2c034473e1d5ce563f278b979ca2eae
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-20 22:24:29 +0530

    update This program defines a list of numbers and uses a for loop to print each element of the list. in that way we can iterate through the list and perform some operations on it.

--- for.py
+++ for.py
@@ -1,4 +1,4 @@
-# This program defines a list of numbers and uses a for loop to print each element of the list.
+# This program defines a list of numbers and uses a for loop to print each element of the list. in that way we can iterate through the list and perform some operations on it.
 list = [1,2,3,4,5]
 for i in list:
     print(i)
