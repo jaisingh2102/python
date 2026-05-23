a2ba2f93a1fc1b67c0666ee8c9b558a17013861d
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-23 23:22:24 +0530

    The code below prints the numbers from 100 to 1 in reverse order.

--- practice2.py
+++ practice2.py
@@ -1,2 +1,3 @@
+# The code below prints the numbers from 100 to 1 in reverse order. It uses a for loop that iterates through a range of numbers starting from 100 down to (but not including) 0, with a step of -1. In each iteration, it prints the current number 'i'.
 for i in range(100,0,-1):
     print(i)
