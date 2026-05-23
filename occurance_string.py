fc1bd12726e99e346159dc8de363fd7197be154d
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-23 23:14:56 +0530

    The count() method in Python is used to count the number of occurrences of a substring in a string.

--- occurance_string.py
+++ occurance_string.py
@@ -1,3 +1,4 @@
+# The count() method in Python is used to count the number of occurrences of a substring in a string. It takes the substring as an argument and returns the number of times it appears in the string.
 str = "hello $my na$me is $aman$"
 count = str.count("$")
 print("Number of occurrences of '$':", count)
