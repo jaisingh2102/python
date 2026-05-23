0bfb92dfe078d41a9b09fae3773be249f6d31705
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-23 23:20:33 +0530

    The code below prints the numbers from 1 to 100.

--- practice1.py
+++ practice1.py
@@ -1,2 +1,3 @@
+# The code below prints the numbers from 1 to 100. It uses a for loop that iterates through a range of numbers starting from 1 up to (but not including) 101. In each iteration, it prints the current number 'i'.
 for i in range(1,101):
     print(i)
