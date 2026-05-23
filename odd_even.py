968355742b340da68744a09450f12745dc01481b
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-23 23:15:52 +0530

    The code below checks if a given number is even or odd.

--- odd_even.py
+++ odd_even.py
@@ -1,3 +1,4 @@
+# The code below checks if a given number is even or odd. It takes input from the user, converts it to an integer, and then uses the modulus operator to determine if the number is divisible by 2 (even) or not (odd).
 num = int(input("Enter a number: "))
 if num % 2 == 0:
     print("The number is even.")
