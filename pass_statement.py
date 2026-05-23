03640c082b2c0eed613341d90c0c6e486515c497
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-23 23:17:06 +0530

    pass statement in python is used when we want to write a code but we don't want to execute it.

--- pass_statement.py
+++ pass_statement.py
@@ -1,6 +1,5 @@
 #pass statement in python is used when we want to write a code but we don't want to execute it. 
 #It is a null statement that does nothing when executed. 
 #It is often used as a placeholder for code that will be written later or to create an empty block of code.
-#Example 1: Using pass statement in a function
 for i in range(10):
     pass # This will do nothing and the loop will continue to the next iteration
