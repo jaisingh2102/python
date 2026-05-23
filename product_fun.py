fe90c7175951167902d0367ed8b019d3333af60c
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-23 23:25:41 +0530

    The code below defines a function called 'calc_product' that takes two parameters, 'a' and 'b'.

--- product_fun.py
+++ product_fun.py
@@ -1,3 +1,4 @@
+# The code below defines a function called 'calc_product' that takes two parameters, 'a' and 'b'. It calculates the product of these two numbers and returns the result. The code then prompts the user to enter two numbers, calls the 'calc_product' function with those numbers, and prints the result in a formatted string.
 def calc_product(a , b):
     c = a * b
     return c
