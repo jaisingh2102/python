2358c90fe68d23249f845208bce15287cadf7598
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-24 22:56:07 +0530

    update This program takes three numbers as input and stores them in a list.

--- qs1.py
+++ qs1.py
@@ -1,3 +1,4 @@
+# This program takes three numbers as input and stores them in a list.
 a = input("enter the first number: ")
 b = input("enter the second number: ")
 c = input("enter the third number: ")
