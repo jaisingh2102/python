17408a2a3935f8cd178ea8d2da3e105907d4b7c9
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-25 22:38:21 +0530

    update Create a set with the following values: python, java, c++, python, javascript, java, python, java, c++, c.

--- qs6.py
+++ qs6.py
@@ -1,3 +1,4 @@
+# Create a set with the following values: "python", "java", "c++", "python", "javascript", "java", "python", "java", "c++", "c". Then, print the length of the set and the whole set.
 set = {"python","java","c++","python","javascript","java","python","java","c++","c"}
 length = len(set)
 print(length) #prints the length of the set, which is 5 because sets do not allow duplicate values
