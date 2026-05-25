ace2b4306bd0e3f96ae093e71dfd6eb2c7dde55b
jaisingh2102 <singhamanjay102@gmail.com>
2026-05-25 22:41:59 +0530

    update Create a set with the following values: 9, 9.0, 9, 9.0. Then, print the set.

--- qs8.py
+++ qs8.py
@@ -1,3 +1,4 @@
+# Create a set with the following values: 9, "9.0", 9, "9.0". Then, print the set.
 set = {9,"9.0"}
 print(set)#prints the set, which contains the integer 9 and the string "9.0" as two distinct elements
 # OR built in methods of set
