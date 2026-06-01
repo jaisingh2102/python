e767cac0876dca15d05ec6c210e42a5e4a8852a0
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-01 23:08:30 +0530

    update tuple:- a built-in data structure that stores multiple elements of different data types in a single variable.

--- tuple.py
+++ tuple.py
@@ -1,6 +1,5 @@
 #tuple:- a built-in data structure that stores multiple elements of different data types in a single variable.
-#it is immutable, ordered,and allows duplicate elements.
-#tuples are defined using parentheses () and elements are separated by commas.
+#it is immutable, ordered,and allows duplicate elements.tuples are defined using parentheses () and elements are separated by commas.
 tup = (1,2,3,2,1,5)
 print(type(tup))
 print(tup)
