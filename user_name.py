8f41931cb297648eaefe7561bd9c4bc6ca8526fd
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-02 23:00:30 +0530

    update This program takes the user's name as input and prints it along with its length.

--- user_name.py
+++ user_name.py
@@ -1,4 +1,5 @@
-str = input("Enter your name: ")
-print("Your name is: " , str)
-length = len(str)
-print("Length of your name is: " , length)
+# This program takes the user's name as input and prints it along with its length.
+name = input("Enter your name: ")
+print("Your name is: ", name)
+length = len(name)
+print("Length of your name is: ", length)
