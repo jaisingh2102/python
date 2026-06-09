0ee7685c07bee98d836f3d146f02f1a2fe334679
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-09 22:30:52 +0530

    update Program to print multiplication table of a number using while loop and taking input from user

--- whlie_3.py
+++ whlie_3.py
@@ -1,4 +1,4 @@
-# Program to print multiplication table of a number using while loop
+# Program to print multiplication table of a number using while loop and taking input from user
 a = int(input("enter a number: "))
 b = 1
 while b<=10:
