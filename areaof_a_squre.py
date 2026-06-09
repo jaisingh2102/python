17fcbe4c647aaf6f2d9cec14e5f807c09c0f085a
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-09 22:35:24 +0530

    update  Program to calculate the area of a square by taking the side length as input from the user.

--- areaof_a_squre.py
+++ areaof_a_squre.py
@@ -1,4 +1,4 @@
-# Program to calculate the area of a square
+# Program to calculate the area of a square by taking the side length as input from the user.
 a = float(input("enter the side of the square"))
 area = a*a
 print("area of a square is:", area)
