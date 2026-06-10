62f535f44e7fc1c2ee33a7fff88e32db2409bdb5
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-10 22:17:25 +0530

    update  Average Calculator by 2 numbers

--- avg.py
+++ avg.py
@@ -1,4 +1,4 @@
-# This program calculates the average of two numbers entered by the user.
+# Average Calculator by 2 numbers
 a = float(input("enter the first number"))
 b = float(input("enter the second number"))
 avg = (a+b)/2
