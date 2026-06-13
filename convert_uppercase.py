e8260e3d405ebebb9a27df51a5cd5618c52ee2fe
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-13 22:28:17 +0530

    update This code demonstrates how to convert a list of names to uppercase using the 'map' function and the 'str.upper' method.

--- convert_uppercase.py
+++ convert_uppercase.py
@@ -1,4 +1,4 @@
-# Convert a list of strings to uppercase
+# This code demonstrates how to convert a list of names to uppercase using the 'map' function and the 'str.upper' method. The 'map' function applies the 'str.upper' method to each element in the 'names' list, resulting in a new list where all the names are in uppercase. Finally, it prints the resulting list of uppercase names.
 names = ["himanshu", "anmol", "prashant", "aman", "jay"]
 
 result = list(map(str.upper, names))
