f3af6b96dd728dc785464c5fd9b3e6dc521b27a0
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-13 22:27:14 +0530

    update The 'continue' statement in Python is used to skip the current iteration of a loop and move on to the next iteration.

--- continue.py
+++ continue.py
@@ -1,4 +1,4 @@
-# This code demonstrates the use of the 'continue' statement in a while loop.
+# The 'continue' statement in Python is used to skip the current iteration of a loop and move on to the next iteration. In this code, we have a while loop that iterates from 1 to 10. When the value of 'a' is equal to 5, the 'continue' statement is executed, which causes the loop to skip the print statement for that iteration and move on to the next iteration. As a result, the number 5 will not be printed in the output.
 a = 1
 while a <= 10:
     if a == 5:
