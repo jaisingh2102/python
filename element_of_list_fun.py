76cc165a517f3563270a7f9cfed1e9a2e1a6255e
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-14 23:30:33 +0530

    update  This program demonstrates how to define a list of sports and print its elements using a function in Python.

--- element_of_list_fun.py
+++ element_of_list_fun.py
@@ -1,4 +1,4 @@
-# This program defines a list of sports and a function to print the elements of the list. The function takes a list as an argument and prints its contents. Finally, the function is called with the 'sports' list to display the elements.
+# This program demonstrates how to define a list of sports and print its elements using a function in Python.
 sports = ["cricket", "football", "tennis", "badminton", "hockey"]
 def print_list(list):
     print("the elements of the list are:", list)
