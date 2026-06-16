be6632d45219f060956b441ef57d6925f50f195e
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-16 23:39:35 +0530

    update   Factorial of a number using iteration

--- factorial.py
+++ factorial.py
@@ -1,4 +1,4 @@
-# This program calculates the factorial of a given number by the user.
+# Factorial of a number using iteration
 n = int(input("enter a number: "))
 factorial = 1
 for i in range(1,n+1):
