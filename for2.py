46c8900c63f8549e34a9767e79bf567d8b50d73e
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-17 23:42:30 +0530

    This is a simple for loop that searches for a number in a list of perfect squares and prints the index of the number if found, or a message indicating that the number was not found.

--- for2.py
+++ for2.py
@@ -1,4 +1,4 @@
-# This program searches for a specific number in a list and prints its index if found.
+# This is a simple for loop that searches for a number in a list of perfect squares and prints the index of the number if found, or a message indicating that the number was not found.
 list = [1,4,9,16,25,36,49,64,81,100]
 x = int(input("enter a number to search: "))
 for i in range(len(list)):
