2a0ab99a3f9eb471a0c04c292c5576fa36e95691
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-17 23:41:29 +0530

    This is a simple for loop that iterates through a list of perfect squares and prints each number to the console.

--- for1.py
+++ for1.py
@@ -1,4 +1,4 @@
-# This program defines a list of perfect squares and uses a for loop to print each element.
+# This is a simple for loop that iterates through a list of perfect squares and prints each number to the console.
 list = [1,4,9,16,25,36,49,64,81,100]
 for i in list:
     print(i)
