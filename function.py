d05c71d386211e2da404a109729396025059550c
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-18 23:39:42 +0530

    function to calculate sum of two numbers

--- function.py
+++ function.py
@@ -1,4 +1,4 @@
-# This program defines a function to calculate the sum of two numbers.
+#function to calculate sum of two numbers
 def sum(a,b):#function definition with parameters a and b
     s = a+b
     return s
