d253839d6639cba02326a752cae48062de474401
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-20 22:43:11 +0530

    This code defines a function calc_length that calculates and prints the length of a given list.

--- length_of_list_fun.py
+++ length_of_list_fun.py
@@ -1,4 +1,4 @@
-# Function to calculate the length of a list
+# This code defines a function `calc_length` that calculates and prints the length of a given list. It then creates two lists, `cities` and `subjects`, and calls the `calc_length` function for each list to display their lengths.
 cities = ["new york", "dilhi","london","mumbai","paris","sydney"]
 subjects =["java","python","c++","javascript","ruby","go"]
 def calc_length(list):
