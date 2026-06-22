3c17432320832e7ab94adbca52658dce0a66289c
jaisingh2102 <singhamanjay102@gmail.com>
2026-06-22 18:35:58 +0530

    list using recursion

--- list_recursion.py
+++ list_recursion.py
@@ -1,4 +1,4 @@
-# This code defines a function 'print_list' that takes a list of 'gods' as input and prints each god's name recursively. The function checks if the list is empty; if it is, it returns. Otherwise, it prints the first element of the list and then calls itself with the rest of the list (excluding the first element). Finally, it calls the function with a predefined list of gods and prints the output.
+# list using recursion
 gods = ["rama","krishna","shiva","vishnu","hanuman","ganesha"]
 def print_list(gods):
     if len(gods) == 0:
